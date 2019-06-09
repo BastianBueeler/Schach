8575218412a76dc6f4cff79bfd58e7e8a1d9c04e
Bastian Bueeler <bastian.bueeler@gmail.com>
2019-06-09 19:56:59 +0200

    StartGUI überarbeitet und funktionsfähigkeit mit Controller erstellt

---
+++ src/grafik/MainController.java
@@ -0,0 +1,5 @@
+package grafik;
+
+public class MainController {
+
+}
---
+++ src/grafik/StartApplication.java
@@ -0,0 +1,29 @@
+package grafik;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class StartApplication extends Application {
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		stage.getIcons().add(new Image("/grafik/logo.png"));
+		stage.setTitle("Schachsoftware");
+
+		Parent root = FXMLLoader.load(getClass().getResource("StartFXML.fxml"));
+
+		Scene scene = new Scene(root);
+
+		stage.setScene(scene);
+		stage.show();
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
